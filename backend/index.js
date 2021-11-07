299e0e14cbc0c27bec69dec91f95dced701e697e
Sharegh Yusefi <48734749+ShareghYusefi@users.noreply.github.com>
2021-11-07 14:11:17 -0700

    Environment Variable Configuration Example - Config Package - Fixed

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,8 @@
+// Import environment config object
+const config = require('config');
+
 const express = require('express');
 const app = express();
-const config = require('./config');
 const Task = require ('./models/Tasks');
 // Allow different URL's to communicate with our server through CORS middleware
 const cors = require('cors');
@@ -8,6 +10,13 @@ app.use(cors());
 // Middleware to handle json and url encoded requests
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
+
+// Test different configurations
+console.log('Application Name: ' + config.get('name'));
+console.log('Mail Server: ' + config.get('mail.host'));
+// Stored environment variable can be mapped to custom envrionment variables define by config package for referening through the config object
+// console.log('Mail Password: ' + config.get('mail.password'));
+
 
 app.get('/home', (req, res) => {
     res.send('Hello World');
@@ -43,7 +52,7 @@ app.delete('/:id', (req, res)=>{
         return res.status(500).send(error);
     });
 })
-
+// global process object has property variable env 
 app.listen(process.env.PORT || 3000, () => {
     console.log('Server is on port 3000...');
 });
